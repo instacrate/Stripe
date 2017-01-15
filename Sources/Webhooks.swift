e5670bd078e3f71fa6cee407551d96519ff97f45
Hakon Hanesand <hakon@abello.no>
2017-01-15 11:53:57 -0600

    Make web hooks simpler

--- Sources/Webhooks.swift
+++ Sources/Webhooks.swift
@@ -43,7 +43,7 @@ private func parseEvent(fromRequest request: Request) throws -> (WebhookResource
     let _action = components[components.count - 1].lowercased()
 
     guard let resource = WebhookResource(rawValue: _resource), let action = WebhookAction(rawValue: _action) else {
-        throw Abort.custom(status: .noContent, message: "Unsupported event type.")
+        throw Abort.custom(status: .internalServerError, message: "Unsupported event type.")
     }
 
     return (resource, action)
@@ -55,16 +55,14 @@ public class StripeWebhookManager: RouteCollection {
 
     public typealias Wrapped = HTTP.Responder
 
-    fileprivate var webhookHandlers: [WebhookResource : [WebhookAction : [(WebhookResource, WebhookAction, Request) throws -> (Response)]]] = [:]
+    fileprivate var webhookHandlers: [WebhookResource : [WebhookAction : (WebhookResource, WebhookAction, Request) throws -> (Response)]] = [:]
 
     public func registerHandler(forResource resource: WebhookResource, action: WebhookAction, handler: @escaping (WebhookResource, WebhookAction, Request) throws -> Response) {
 
         var resourceHanderGroup = webhookHandlers[resource] ?? [:]
-        var actionHandlerGroup = resourceHanderGroup[action] ?? []
 
-        actionHandlerGroup.append(handler)
+        resourceHanderGroup[action] = handler
 
-        resourceHanderGroup[action] = actionHandlerGroup
         webhookHandlers[resource] = resourceHanderGroup
     }
 
@@ -75,14 +73,11 @@ public class StripeWebhookManager: RouteCollection {
             webhook.post() { request in
                 let (resource, action) = try parseEvent(fromRequest: request)
 
-                guard let handlers = self.webhookHandlers[resource]?[action] else {
-                    return Response(status: .noContent)
+                guard let handler = self.webhookHandlers[resource]?[action] else {
+                    return Response(status: .notImplemented)
                 }
 
-                let responses = try handlers.map { try $0(resource, action, request) }
-                let failingResponses = responses.filter { !(200..<300 ~= $0.status.statusCode) }
-
-                return failingResponses.count > 0 ? failingResponses.first! : Response(status: .noContent)
+                return try handler(resource, action, request)
             }
         }
     }
